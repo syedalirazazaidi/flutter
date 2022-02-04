0f77e9a3ca4d741e279de01442f63380e7f85acb
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-02-04 11:03:27 +0500

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Text('my first Name!')
-
-        ),
-        body:
-          Center(
-            child: Image(
-              image: AssetImage('image/bulb.jpg'),
-            ),
-          ),
-
-
-      ),
-    )
+    MyApp()
   );
 }
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:
+      // SafeArea(child:
+      Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            // backgroundColor: Colors.deepOrange,
+            title: Text('my first Name!')
 
+        ),
+        body: SafeArea(
+          child: Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('hello workd',style: TextStyle(fontSize: 16,color: Colors.blue,fontWeight: FontWeight.w800,)),
+              Image(
+                  width: 150,
+                  height: 150,
+                  image: AssetImage('image/bulb.jpg')),
+            ],
+          ),
+
+          // child: Center(
+          //   child: Container(
+          //     child: Text('Hello World'),
+          //     padding: EdgeInsets.symmetric(vertical: 24, horizontal: 22),
+          //
+          //    margin: EdgeInsets.only(left: 10, top: 10),
+          //
+          //     color: Colors.pinkAccent,
+          //     width: 150,
+          //     height: 70,
+          //   ),
+          // ),
+        ),
+
+      ),
+    // )
+    );
+  }
+}
